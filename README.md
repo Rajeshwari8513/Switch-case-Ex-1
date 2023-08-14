8d442eeafd9e9801753891272df4668061f6e683
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-14 11:17:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Switch-case-Ex-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-l331pl)
---
+++ index.js
@@ -0,0 +1,35 @@
+let dayOfWeek = 6;
+let dayName;
+
+switch (dayOfWeek){
+    case 0 :
+        dayName = "Sunday";
+        console.log("It's a week-end")
+        break;
+    case 1 :
+        dayName = "Monday";
+        console.log("It's a weekday")
+        break;
+    case 2 :
+        dayName = "Tuesday";
+        console.log("It's a weekday")
+        break;
+    case 3 :
+        dayName = "Wednesday";
+        console.log("It's a weekday")
+        break;
+    case 4 :
+        dayName = "Thrusday";
+        console.log("It's a weekday")
+        break;
+    case 5 :
+        dayName = "Friday";
+        console.log("It's a weekday")
+        break;
+    case 6 :
+        dayName = "Saturday";
+        console.log("It's a week-end")
+        break;
+    default :
+    console.log("Invalid day")
+}
